55df0b51fde259459e70fbcf2b380e4cce06d21b
Justin Cabral <70143234+justinhcabral@users.noreply.github.com>
2025-10-20 03:59:12 +0800

    Create json file if it doesn't exist

--- index.js
+++ index.js
@@ -1,2 +1,55 @@
 #!/usr/bin/env node
 
+// json file
+/*
+Check first if json file has been created, create one if not
+
+when creating json file
+
+*/
+
+const fs = require("fs"); //file system
+const path = require("path");
+
+const filePath = path.join(__dirname, "tracker.json");
+
+const data = {
+  notDone: [
+    {
+      taskId: "",
+      taskName: "",
+    },
+  ],
+  inProgress: [
+    {
+      taskId: "",
+      taskName: "",
+    },
+  ],
+  done: [
+    {
+      taskId: "",
+      taskName: "",
+    },
+  ],
+};
+const jsonString = JSON.stringify(data, null, 2);
+
+if (!fs.existsSync(filePath)) {
+  fs.writeFile("tracker.json", jsonString, (err) => {
+    err ? console.log(err) : console.log("json saved successfully");
+  });
+}
+
+/* structure of task tracker
+
+notDone : {
+  id: ,
+  taskname: },
+
+approach to turning a json file into a database
+
+
+
+
+*/
