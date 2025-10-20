266c78ae7687d110c9f07783ef90d1dcecba66e6
Justin Cabral <70143234+justinhcabral@users.noreply.github.com>
2025-10-20 16:53:54 +0800

    Rename index to task-cli

--- task-cli.js
+++ task-cli.js
@@ -1,18 +1,12 @@
 #!/usr/bin/env node
-
-// json file
-/*
-Check first if json file has been created, create one if not
-
-when creating json file
-
-*/
+// the shebang line above tells us what interpreter to use, in this case it's node
 
 const fs = require("fs"); //file system
-const path = require("path");
+const path = require("path"); // import path
 
-const filePath = path.join(__dirname, "tracker.json");
+const filePath = path.join(__dirname, "tracker.json"); // gets the path to the tracker.json file
 
+// empty data that initializes tracker.json
 const data = {
   notDone: [
     {
@@ -41,15 +35,14 @@ if (!fs.existsSync(filePath)) {
   });
 }
 
-/* structure of task tracker
+/* 
+after initializing tracker.json, we need to create functions for the following things
 
-notDone : {
-  id: ,
-  taskname: },
+1. adding tasks
+2. updating tasks
+3. deleting tasks
 
-approach to turning a json file into a database
-
-
-
+steps to add task
+1. when using
 
 */
