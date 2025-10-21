4f385288766db3ef9e3b5122a668326bcdbea0ee
Justin Cabral <70143234+justinhcabral@users.noreply.github.com>
2025-10-22 01:19:12 +0800

    Implement Read and Write Functions for json file

--- index.js
+++ index.js
@@ -3,91 +3,132 @@
 
 const fs = require("fs"); //file system
 const path = require("path"); // import path
-
 const filePath = path.join(__dirname, "tracker.json"); // gets the path to the tracker.json file
 
-let datares, trackerContent, tasks; // to access the contents of the json files
-
-// empty data that initializes tracker.json
-const trackerTemplate = [
-  {
-    id: "",
-    description: "",
-    status: "",
-    createdAt: "",
-    updatedAt: "",
-  },
-];
-const jsonString = JSON.stringify(trackerTemplate, null, 2);
-
-if (!fs.existsSync(filePath)) {
-  fs.writeFile("tracker.json", jsonString, (err) => {
-    err ? console.log(err) : console.log("json saved successfully");
-  });
-} else {
-  datares = fs.readFileSync(filePath);
-  trackerContent = JSON.parse(datares);
-  tasks = trackerContent.tasks;
-  // console.log(trackerContent);
+function readTracker() {
+  try {
+    jsonstring = fs.readFileSync(filePath, "utf-8");
+    return JSON.parse(jsonstring);
+  } catch (error) {
+    console.log("Error reading tracker: ", error.message);
+    return [];
+  }
 }
 
-/*
-after initializing tracker.json, we need to create functions for the following things
+function writeTracker(content, successMessage) {
+  contentString = JSON.stringify(content);
+  try {
+    fs.writeFileSync(filePath, contentString);
+    console.log("data written successfully");
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-1. adding tasks
-2. updating tasks
-3. deleting tasks
+// test run
 
-steps to add task
-Input: task,
-Process:
-1.) read entire json file
-2.) parse json file into javascript object
-3.) modify the javascript object
-4.) convert it back to json
-5.) write entire file of json
-6.) console.log(`Task added successfully (ID:${notDone.id})`)
-Output: Task added successfully (ID:1)
-*/
+const data = [{ id: 1, description: "dick" }];
 
-const add = (task) => {
-  let key;
-  const d = new Date();
-  let obj = trackerContent.reduce((a, c) => ((a[c.id] = c), a), {}); // to turn ids into keys
-  largest_id = Math.max(...Object.keys(obj));
+writeTracker(data, "");
+console.log(readTracker());
 
-  key = largest_id + 1;
-  const addTask = {
-    id: key,
-    description: task,
-    status: "Not Done",
-    createdAt: `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`,
-    updatedAt: `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`,
-  };
-  trackerContent.push(addTask);
+// steps to add task
+// Input: task,
+// Process:
+// 1.) read entire json file
+// 2.) parse json file into javascript object
+// 3.) modify the javascript object
+// 4.) convert it back to json
+// 5.) write entire file of json
+// 6.) console.log(`Task added successfully (ID:${notDone.id})`)
+function add(taskDescription) {
+  let trackerContent = readTracker();
+}
 
-  trackerContentString = JSON.stringify(trackerContent, null, 2);
+// let trackerContent; // to access the contents of the json files
 
-  fs.writeFile("tracker.json", trackerContentString, (err) => {
-    err
-      ? console.log(err)
-      : console.log(`Task Added Successfully (ID: ${key})`);
-  });
-};
-add("Goon to lebron");
+// // empty data that initializes tracker.json
+// const trackerTemplate = [{}];
+// const jsonString = JSON.stringify(trackerTemplate, null, 2);
 
-/*
-implement update
-Input: task, id number of task
-Process:
-1.) read entire json file
-2.) parse json file into javascript object
-3.) modify the javascript object
-4.) convert it back to json
-5.) write entire file of json
-6.) console.log(`Task added successfully (ID:${notDone.id})`)
-Output: Task added successfully (ID:1)
- */
+// if (!fs.existsSync(filePath)) {
+//   fs.writeFile("tracker.json", jsonString, (err) => {
+//     err ? console.log(err) : console.log("json saved successfully");
+//   });
+// }
 
-// TODO: change object structure of tracker.json
-// why: because there are required properties that you skipped over
+// const datares = fs.readFileSync(filePath);
+// trackerContent = JSON.parse(datares);
+
+// /*
+// after initializing tracker.json, we need to create functions for the following things
+
+// 1. adding tasks
+// 2. updating tasks
+// 3. deleting tasks
+
+// steps to add task
+// Input: task,
+// Process:
+// 1.) read entire json file
+// 2.) parse json file into javascript object
+// 3.) modify the javascript object
+// 4.) convert it back to json
+// 5.) write entire file of json
+// 6.) console.log(`Task added successfully (ID:${notDone.id})`)
+// Output: Task added successfully (ID:1)
+// */
+
+// const add = (task) => {
+//   let key;
+//   const d = new Date();
+
+//   if (trackerContent.length === 0) {
+//     key = 1;
+//   } else {
+//     // FIlter out any invalid entries and get numerid IDs
+//     const validIds = trackerContent
+//       .map((item) => Number(item.id))
+//       .filter((id) => !isNaN(id) && id > 0);
+
+//     const largest_id = validIds.length > 0 ? Math.max(...validIds) : 0;
+//     key = largest_id + 1;
+//   }
+
+//   const addTask = {
+//     id: key,
+//     description: task,
+//     status: "Not Done",
+//     createdAt: `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`,
+//     updatedAt: `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`,
+//   };
+//   trackerContent.push(addTask);
+
+//   trackerContentString = JSON.stringify(trackerContent, null, 2);
+
+//   fs.writeFile("tracker.json", trackerContentString, (err) => {
+//     err
+//       ? console.log(err)
+//       : console.log(`Task Added Successfully (ID: ${key})`);
+//   });
+// };
+// add("Goon to lebron");
+
+// /*
+// implement update
+// Input: task, id number of task
+// Process:
+// 1.) read entire json file
+// 2.) parse json file into javascript object
+// 3.) modify the javascript object
+// 4.) convert it back to json
+// 5.) write entire file of json
+// 6.) console.log(`Task added successfully (ID:${notDone.id})`)
+// Output: Task added successfully (ID:1)
+//  */
+
+// // const update = (taskId) => {
+
+// // }
+
+// // console.log(tasks[1]);s
---
+++ refactorPlan.md
@@ -0,0 +1,10 @@
+Primary Functions
+
+1. create a function that reads the contents of tracker.json
+2. create a function that writes to the contents of tracker.json
+   Secondary Functions
+3. create a function that adds task to json
+4. create a function that updates description of task in json
+5. create function that marks task in json as in progress or done
+6. create a function that removes a task from json (delete)
+7. create a function that lists all functions with a certain status (todo, in-progress, done)
